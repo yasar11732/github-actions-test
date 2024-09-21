ae96106c0dc78ed0f7c193f8b50cb559cc60cf8e
Yaşar Arabacı <yasar11732@gmail.com>
2024-09-21 15:14:57 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <libxml/xmlwriter.h>
+
+int main(void) {
+    xmlTextWriterPtr writer;
+    writer = xmlNewTextWriterFilename("test.xml", 0);
+    xmlTextWriterStartDocument(writer, NULL, "UTF-8", NULL);
+    xmlTextWriterStartElement(writer, "Powersettings");
+    xmlTextWriterWriteElement(writer, "PowerScheme", "Testing"); xmlTextWriterEndElement(writer);
+    xmlTextWriterWriteElement(writer, "CPUSpeed", "Adaptive"); xmlTextWriterEndElement(writer);
+    xmlTextWriterEndElement(writer);
+    xmlTextWriterEndDocument(writer);
+    xmlFreeTextWriter(writer);
+}
